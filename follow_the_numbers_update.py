c1806d95e66a6d2ed5526c1adfc4e05b38a3d82b
Tatiana Turin <tatiana.turin@gmail.com>
2022-07-13 22:36:12 -0400

    c

--- follow_the_numbers_update.py
+++ follow_the_numbers_update.py
@@ -5,6 +5,18 @@ The game will finish once all of the dots are connected. If you make a mistake, 
 have to start over again 
 
 would this project be an introduction to arrays? 
+
+Hacks and Tweaks 
+
+add more dots 
+
+create a "next level"
+
+set the number of attempts and print out "game over"
+
+make a function so that the dots do not overlap the previous one 
+
+
 """
 
 #set our vars
@@ -16,15 +28,28 @@ dots = [] #will be a list of actors
 lines = []
 next_dot = 0
 current_dot=0
+attempts = 3
+end_game = False
+
+
+def place_dots(actor,index):
+    global dots
+    actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
+    if index!=0:
+        if dots[index-1].collidepoint(dots[index].pos[0],dots[index].pos[1]):
+            actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
+
 
 #set up the actors with a list
-for dot in range (0,10):
+for i in range (10):
     actor = Actor("dot")
-    actor.pos = random.randint(20, WIDTH-20), random.randint(20,HEIGHT-20)
+    place_dots(actor,i)
+    #actor.pos = random.randint(20, WIDTH-20), random.randint(20,HEIGHT-20)
     dots.append(actor) #add this actor object to our list
 
 def draw():
     screen.fill("black")
+    screen.draw.text("Attempts: {}".format(attempts), color="white",topleft=(10,10))
     number = 1
 
     #draw the dots
@@ -33,17 +58,16 @@ def draw():
         dot.draw()
         number +=1
 
-    #or
-    """for i in range(dots.len):
-        screen.draw.text(i+1,(dot.pos[0],dot.pos[1]+12))
-        dot.draw()"""
-
     #draw the lines
     for line in lines:
         screen.draw.line(line[0],line[1], (100,0,0)) #line function line(x,y,(rgb values))
 
+    if end_game:
+        screen.clear()
+        screen.draw.text('Game Over', color='white', topleft=((WIDTH/2)-40,HEIGHT/2))
+
 def on_mouse_down(pos):
-    global next_dot, lines, current_dot
+    global next_dot, lines, current_dot, attempts
 
     if dots[current_dot].collidepoint(pos):
         if current_dot == 0: #if user clicks on the 1 dot, dont do anythhing
@@ -55,21 +79,29 @@ def on_mouse_down(pos):
         #user connected incorrectly
         lines=[]
         current_dot=0
+        attempts-=1
+        if attempts == 0:
+            game_over()
+    print(len(dots))
+    if current_dot == len(dots):  # you reached the last dot
+        next_level()
 
 
-"""    #if mouse position is on the next dot in the sequence
-    if dots[next_dot].collidepoint(pos):
-        if next_dot: # an integer as a conditional? Still confused but basically means once the mosue clicks on the dot, if statement checks if the player clicked on the first dot
-            print("next dot {}".format(next_dot))
-            lines.append( (dots[next_dot-1].pos,dots[next_dot].pos) )
-            #print (lines[len(lines)-1])
-        next_dot+=1
-    else: #start over
-        lines = []
-        next_dot=0"""
+def game_over():
+    global end_game
+    end_game = True
 
-    #print (lines) [((326.0, 169.0), (322.0, 172.0)), ((322.0, 172.0), (193.0, 216.0)), ((193.0, 216.0), (77.0, 309.0))]
-    #the lines [] is a list of lists that contain two coordiante pairs [ [ [0],[0] ], [ [0],[[0] ]   ]
+def next_level():
+    global current_dot,lines,dots
+    lines = []
+    current_dot=0
+    dots = []
+
+    for dot in range(0, 15):
+        actor = Actor("dot")
+        actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
+        dots.append(actor)  # add this actor object to our list
+
 
 
 pgzrun.go()
