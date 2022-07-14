4db20e921b8a36e9bd19d22b38d276c73b9d393c
tatiana <tatiana.turin@gmail.com>
2022-07-14 16:29:55 -0400

    c

--- follow_the_numbers_update.py
+++ follow_the_numbers_update.py
@@ -1,4 +1,4 @@
-import pgzrun, random
+import pgzrun, random, math
 """Project No. Follow the Numbers 
 Ten dots appear at random positions, each with a number next to it. Click on the dots in the correct order to connect them.
 The game will finish once all of the dots are connected. If you make a mistake, all the lines will disapear and you will
@@ -35,10 +35,17 @@ end_game = False
 def place_dots(actor,index):
     global dots
     actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
-    if index!=0:
-        if dots[index-1].collidepoint(dots[index].pos[0],dots[index].pos[1]):
-            actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
-
+    x1 = dots[index-1].pos[0]
+    x2 = actor.pos[0]
+    y1 = dots[index-1].pos[1]
+    y2 = actor.pos[1]
+    dist = math.sqrt((x2-x1)^2+(y2-y1)^2)
+    while True:
+        if index!=0:
+            if dist <10:
+                actor.pos = random.randint(20, WIDTH - 20), random.randint(20, HEIGHT - 20)
+            else:
+                break
 
 #set up the actors with a list
 for i in range (10):
@@ -46,6 +53,7 @@ for i in range (10):
     place_dots(actor,i)
     #actor.pos = random.randint(20, WIDTH-20), random.randint(20,HEIGHT-20)
     dots.append(actor) #add this actor object to our list
+    place_dots(actor,i)
 
 def draw():
     screen.fill("black")
